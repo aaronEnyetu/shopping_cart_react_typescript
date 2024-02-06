de4abbcb0521876f1144e33ec483ef7446c6c4be
Aaron Enyetu <aaronenyetu@gmail.com>
2024-02-05 16:08:07 -0800

    created pages and navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import cart from '/public/cart.svg'
+
+const Navbar = () => {
+  return (
+      <NavbarBs className='bg-white shadow-sm mb-3'>
+          <Container>
+              <Nav className='me-auto'>
+                  <Nav.Link to='/' as={NavLink}>
+                      Home
+                  </Nav.Link>
+                  <Nav.Link to='/store' as={NavLink}>
+                      Store
+                  </Nav.Link>
+                  <Nav.Link to='/about' as={NavLink}>
+                      About
+                  </Nav.Link>
+              </Nav>
+              <Button style={{ width: '3rem', height: '3rem' }}>
+                  {cart}
+
+
+              </Button>
+          </Container>
+    </NavbarBs>
+  )
+}
+
+export default Navbar
